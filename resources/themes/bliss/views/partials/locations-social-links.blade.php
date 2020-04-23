db4aa434823e3f1e77ccf1d7e20d992a7dbedb7b
Vahan <vahan99mk@gmail.com>
2020-04-24 01:46:53 +0400

    locales order by desc

--- resources/themes/bliss/views/partials/locations-social-links.blade.php
+++ resources/themes/bliss/views/partials/locations-social-links.blade.php
@@ -1,6 +1,6 @@
 <ul class="header_social {{isset($class) ? $class : ''}}">
 	<li class="languages">
-        @foreach (core()->getCurrentChannel()->locales as $locale)
+        @foreach (core()->getCurrentChannel()->locales()->orderBy('id', 'desc')->get() as $locale)
             <a href="?locale={{ $locale->code }}" class="arm_icon" style="background-image: url(/storage/{{$locale->locale_image}})"></a>
         @endforeach
     </li>
