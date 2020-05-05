de58492dda361c4f2eeff2ce0ca50818fec49fcc
Vahan <vahan99mk@gmail.com>
2020-05-05 20:35:26 +0400

    changes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-
-App::setLocale('hy');
-
-
 Route::group(['middleware' => ['web', 'theme', 'locale', 'currency']], function () {
     Route::get('/products', 'IndexController@products')->defaults('_config', [ 'view' => 'products' ])->name('products');
     Route::get('/matrix', 'IndexController@matrix')->defaults('_config', [ 'view' => 'matrix' ])->name('matrix');
