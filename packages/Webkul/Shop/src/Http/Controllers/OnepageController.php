632fdae567f114b64cfc5f1f8d01c6bfa7688acb
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-05-13 23:15:09 +0400

    changes

--- packages/Webkul/Shop/src/Http/Controllers/OnepageController.php
+++ packages/Webkul/Shop/src/Http/Controllers/OnepageController.php
@@ -219,21 +219,21 @@ class OnepageController extends Controller
                     'jsonParams' => json_encode(['orderNumber' => $order->cart_id]),
                     'pageView' => 'DESKTOP'
                 ];
-//                $client = new \GuzzleHttp\Client;
-//                $response = $client->post('https://ipay.arca.am/payment/rest/register.do', $api_data);
-//                dd($response, $api_data, $order);
-
-                $ch = curl_init();
-
-                curl_setopt($ch, CURLOPT_URL,"https://ipay.arca.am/payment/rest/register.do");
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($api_data));
-
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-                $server_output = curl_exec($ch);
-                dd($server_output);
-                curl_close ($ch);
+                $client = new \GuzzleHttp\Client;
+                $response = $client->post('https://ipay.arca.am/payment/rest/register.do', $api_data);
+                dd($response, $api_data, $order);
+//
+//                $ch = curl_init();
+//
+//                curl_setopt($ch, CURLOPT_URL,"https://ipay.arca.am/payment/rest/register.do");
+//                curl_setopt($ch, CURLOPT_POST, 1);
+//                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($api_data));
+//
+//                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//
+//                $server_output = curl_exec($ch);
+//                dd($server_output);
+//                curl_close ($ch);
 
 
             }
