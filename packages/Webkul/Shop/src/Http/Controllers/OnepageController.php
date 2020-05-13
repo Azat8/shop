e00dc12710e5763d01cc7c8e32c05574e8382fdc
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-05-14 00:14:53 +0400

    changes

--- packages/Webkul/Shop/src/Http/Controllers/OnepageController.php
+++ packages/Webkul/Shop/src/Http/Controllers/OnepageController.php
@@ -213,7 +213,7 @@ class OnepageController extends Controller
 //                    'currency' => core()->getBaseCurrency()->code,
 //                    'language' => app()->getLocale(),
 //                    'orderNumber' => $order->cart_id,
-                    'password'    => 'mBxYhy5XqQEs887sa'/*config('bank-api.bank_api.password')*/,
+                    'password'    => 'mBxYhy5XqQEs887s'/*config('bank-api.bank_api.password')*/,
 //                    'returnUrl' => urlencode(url()->current()),
                     'userName'    => config('bank-api.bank_api.login'),
 //                    'jsonParams' => json_encode(['orderNumber' => $order->cart_id]),
