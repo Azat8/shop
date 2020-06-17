5e61db5c4f5f5333819934fc9bb1665aac6cb435
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-06-17 15:37:44 +0400

    order by pos

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -199,7 +199,7 @@ class ProductRepository extends Repository
                 });
 
                 return $qb->groupBy('product_flat.id');
-            })->paginate(isset($params['limit']) ? $params['limit'] : 9);
+            })->orderBy('position', 'desc')->paginate(isset($params['limit']) ? $params['limit'] : 9);
 
         return $results;
     }
