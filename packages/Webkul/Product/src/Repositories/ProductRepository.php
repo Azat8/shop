d6a8b57dcd0398cdaa9f74a1dc7635b7a15a1c4c
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-06-19 13:25:47 +0400

    position

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -199,7 +199,7 @@ class ProductRepository extends Repository
                 });
 
                 return $qb->groupBy('product_flat.id');
-            })->orderBy('position', 'asc')->paginate(isset($params['limit']) ? $params['limit'] : 9);
+            })->orderBy('position')->paginate(isset($params['limit']) ? $params['limit'] : 9);
 
         return $results;
     }
