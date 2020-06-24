6031ac5109a137f32d4af864ec70052fa3a3ef37
Azat <mkhitaryan103@gmail.com>
2020-06-24 19:17:39 +0400

    changes

---
+++ database/seeds/PositionSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PositionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $productFlats = \Webkul\Product\Models\ProductFlat::whereLocale('hy')->get(['position', 'product_id'])->toArray();
+		foreach($productFlats as $flat){
+			$finded = \Webkul\Product\Models\ProductAttributeValue::where('product_id', $flat['product_id'])->where('attribute_id', '28')->count();
+				if(!$finded){
+					$data['text_value'] = $flat['position'];
+					$data['product_id'] = $flat['product_id'];
+					$data['attribute_id'] = $flat['attribute_id'];
+					\Webkul\Product\Models\ProductAttributeValue::create($data);
+				}
+		}    
+	}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,3 @@ Route::group(['middleware' => ['web', 'theme', 'locale', 'currency']], function 
     Route::get('/products', 'IndexController@products')->defaults('_config', [ 'view' => 'products' ])->name('products');
 //    Route::get('/matrix', 'IndexController@matrix')->defaults('_config', [ 'view' => 'matrix' ])->name('matrix');
 });
-
-Route::get('position-attributes', function(){
-	$productFlats = \Webkul\Product\Models\ProductFlat::whereLocale('hy')->get(['position', 'product_id'])->toArray();
-	foreach($productFlats as $flat){
-		$finded = \Webkul\Product\Models\ProductAttributeValue::where('product_id', $flat['product_id'])->where('attribute_id', '28')->count();
-		if(!$finded){
-			$data['text_value'] = $flat['position'];
-			$data['product_id'] = $flat['product_id'];
-			$data['attribute_id'] = $flat['attribute_id'];
-			\Webkul\Product\Models\ProductAttributeValue::create($data);
-		}
-	}
-});
