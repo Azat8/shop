73263506a8e075522971e82bfcfbd059933b0801
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-06-24 15:02:59 +0400

    change

--- packages/Webkul/Shop/src/Http/Controllers/OnepageController.php
+++ packages/Webkul/Shop/src/Http/Controllers/OnepageController.php
@@ -211,9 +211,9 @@ class OnepageController extends Controller
                 $formater->setAttribute(\NumberFormatter::FRACTION_DIGITS, 0);
 
                 $totalAmount = $formater->format($order->base_grand_total);
-                dd($totalAmount, (float)$order->base_grand_total);
+
                 $api_data = [
-                    'amount' => $totalAmount,
+                    'amount' => (float)$order->base_grand_total,
                     'currency' => '051',
                     'language' => app()->getLocale(),
                     'orderNumber' => $order->id,
