e35e7ba0db15b4dd1e64df7734288c69292f4b76
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-06-29 01:24:10 +0400

    change

--- resources/themes/bliss/views/cms/page.blade.php
+++ resources/themes/bliss/views/cms/page.blade.php
@@ -12,23 +12,24 @@
 @endsection
 
 @section('content-wrapper')
-    {!! DbView::make($page)->field('html_content')->render() !!}
+    <div id="map"></div>
     <script>
-      function initMap() {
-        var location = {lat: 40.2, lng: 44.55};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
-          center: location
-        });
-        var marker = new google.maps.Marker({
-          position: location,
-          map: map
-        });
-      }
+        // Initialize and add the map
+        function initMap() {
+            // The location of Uluru
+            var uluru = {lat: -25.344, lng: 131.036};
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+                document.getElementById('map'), {zoom: 4, center: uluru});
+            // The marker, positioned at Uluru
+            var marker = new google.maps.Marker({position: uluru, map: map});
+        }
     </script>
-    <script async="" defer=""
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7nSpucMaZeeBRk9JbmLZkVWQx4L8fLgs&amp;callback=initMap"
-            type="text/javascript"></script>
-    </body>
-	
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{env('MAP_API_KEY')}}&callback=initMap"></script>
+    {!! DbView::make($page)->field('html_content')->render() !!}
 @endsection
