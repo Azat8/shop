f68a02ddc4af372f619f27489a62d9d7dd07e980
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-07-14 04:29:07 +0400

    changes

--- resources/themes/bliss/views/emails/layouts/logo.blade.php
+++ resources/themes/bliss/views/emails/layouts/logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{env('APP_URL')}}/themes/bliss/assets/images/hg/hg_logo_{{app()->getLocale()}}.png" alt="{{ config('app.name') }}" style="height: 40px; width: 110px;"/>
+<img src="{{env('APP_URL')}}/themes/bliss/assets/images/hg/hg_logo_{{app()->getLocale()}}.png" alt="{{ config('app.name') }}" style="width: 100px;"/>
