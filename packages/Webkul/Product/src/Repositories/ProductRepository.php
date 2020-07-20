59766a03977cd86545728fbb9169fff447efe577
Azat Mkhitaryan <mkhitaryan103@gmail.com>
2020-07-20 14:52:51 +0400

    changes

--- packages/Webkul/Product/src/Repositories/ProductRepository.php
+++ packages/Webkul/Product/src/Repositories/ProductRepository.php
@@ -263,7 +263,7 @@ class ProductRepository extends Repository
                         ->where('product_flat.new', 1)
                         ->where('product_flat.channel', $channel)
                         ->where('product_flat.locale', $locale)
-                        ->orderBy('product_id', 'desc');
+                        ->orderBy('updated_at', 'desc');
             })->paginate(3);
 
         return $results;
